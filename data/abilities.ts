528a414530a2584cfb2a2c16d6ffbcfab3fc9470
Max2Jeux <max.mercadie@iadfrance.fr>
2025-03-23 10:04:48 +0100

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -109,11 +109,10 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 		num: -100,
 	},	 
 ecoshell: {
-		onDamagingHit(damage, target, source, move) {
+		onDamagingHit(damage, target, source, move) 
 			if (['Water', 'Grass'].includes(move.type)) {
 				this.boost({ spd: 1 });
             (!this.heal(target.baseMaxhp / 4)) 
-		}
 		}
          {
       	onDamagingHit(damage, target, source, move) {
