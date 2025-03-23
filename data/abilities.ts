f18d306eca0f9305bf14973b8673b11525fc6e8c
Max2Jeux <max.mercadie@iadfrance.fr>
2025-03-23 10:23:31 +0100

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -109,17 +109,18 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 		num: -100,
 	},	 
 ecoshell: {
-		onDamagingHit(damage, target, source, move)
-			if (['Fire'].includes(move.type));
-				this.boost({ spe: 6 });
-            (!this.heal(target.baseMaxhp / 4)) 
-	}
+		onDamagingHit(damage, target, source, move) {
+			if (['Water', 'Grass'].includes(move.type)) {
+				this.boost({ spd: 6 });
+            this.heal(target.baseMaxhp / 4);
+			}
+		},
       onDamagingHit(damage, target, source, move) {
-			if (['Rock', 'Ground'].includes(move.type));
+			if (['Rock', 'Ground'].includes(move.type)) {
 				this.boost({ def: 1 });
-            (!this.heal(target.baseMaxhp / 4)) 
-	}
-},
+            this.heal(target.baseMaxhp / 4); 
+			}
+		},
    	flags: {},
 		name: "Eco-Shell",
 		rating: 2,
