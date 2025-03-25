4c49b557c813dbec41fbd58101773f7139f72fc4
Max2Jeux <max.mercadie@iadfrance.fr>
2025-03-25 01:13:44 +0100

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -207,9 +207,9 @@ ecoshell: {
 			const sourceAbility = source.getAbility();
 			if (sourceAbility.flags['cantsuppress'] || sourceAbility.id === 'Normalize') {
 				return;
-			const oldAbility = source.setAbility('mummy', target);
+			const oldAbility = source.setAbility('Normalize', target);
 				if (oldAbility) {
-					this.add('-activate', target, 'ability: Mummy', this.dex.abilities.get(oldAbility).name, `[of] ${source}`);
+					this.add('-activate', target, 'ability: Normalize', this.dex.abilities.get(oldAbility).name, `[of] ${source}`);
 				}
 			}
 		},
