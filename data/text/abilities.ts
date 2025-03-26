d88c34019c7f7b46c890da5a465479a1acf85f48
Max2Jeux <max.mercadie@iadfrance.fr>
2025-03-26 20:42:39 +0100

    Update abilities.ts

--- data/text/abilities.ts
+++ data/text/abilities.ts
@@ -77,7 +77,7 @@ export const AbilitiesText: { [id: IDEntry]: AbilityText } = {
 		gen6: {
 			desc: "This Pokemon's Normal-type moves become Flying-type moves and have their power multiplied by 1.3. This effect comes after other effects that change a move's type, but before Ion Deluge and Electrify's effects.",
 			shortDesc: "This Pokemon's Normal-type moves become Flying type and have 1.3x power.",
-		}
+		},
      },
 	aftermath: {
 		name: "Aftermath",
