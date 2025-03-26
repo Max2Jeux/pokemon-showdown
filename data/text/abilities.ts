263d1ad3bcda86182674179b62d1a17afab6e9c8
Max2Jeux <max.mercadie@iadfrance.fr>
2025-03-26 19:02:18 +0100

    Update abilities.ts

--- data/text/abilities.ts
+++ data/text/abilities.ts
@@ -16,7 +16,21 @@ export const AbilitiesText: { [id: IDEntry]: AbilityText } = {
    silverclaws: {
 		name: "Silver Claws",
 		desc: "This Pokemon's contact moves have their power multiplied by 1.3. This Pokemon's moves and their effects ignore certain Abilities of other Pokemon. The Abilities that can be negated are Armor Tail, Aroma Veil, Aura Break, Battle Armor, Big Pecks, Bulletproof, Clear Body, Contrary, Damp, Dazzling, Disguise, Dry Skin, Earth Eater, Filter, Flash Fire, Flower Gift, Flower Veil, Fluffy, Friend Guard, Fur Coat, Good as Gold, Grass Pelt, Guard Dog, Heatproof, Heavy Metal, Hyper Cutter, Ice Face, Ice Scales, Illuminate, Immunity, Inner Focus, Insomnia, Keen Eye, Leaf Guard, Levitate, Light Metal, Lightning Rod, Limber, Magic Bounce, Magma Armor, Marvel Scale, Mind's Eye, Mirror Armor, Motor Drive, Multiscale, Oblivious, Overcoat, Own Tempo, Pastel Veil, Punk Rock, Purifying Salt, Queenly Majesty, Sand Veil, Sap Sipper, Shell Armor, Shield Dust, Simple, Snow Cloak, Solid Rock, Soundproof, Sticky Hold, Storm Drain, Sturdy, Suction Cups, Sweet Veil, Tangled Feet, Telepathy, Tera Shell, Thermal Exchange, Thick Fat, Unaware, Vital Spirit, Volt Absorb, Water Absorb, Water Bubble, Water Veil, Well-Baked Body, White Smoke, Wind Rider, Wonder Guard, and Wonder Skin. This affects every other Pokemon on the field, whether or not it is a target of this Pokemon's move, and whether or not their Ability is beneficial to this Pokemon.",
-		shortDesc: "This Pokemon's contact moves have their power multiplied by 1.3 + Mold Breaker.
+		shortDesc: "This Pokemon's contact moves have their power multiplied by 1.3. Mold Breaker's effect.
+	},
+   heated: {
+		name: "Heated",
+		desc: "This Pokemon's highest stat is raised by 1 stage after using a move inflicting recoil. If multiple stats are tied, Attack, Defense, Special Attack, Special Defense, and Speed are prioritized in that order.",
+		shortDesc: "This Pokemon's highest stat is raised by 1after using a move with recoil.",
+	},
+  ecoshell: {
+		name: "Eco-Shell",
+		desc: "This Pokemon raises its Special Defense by 1 stage when hit by a Water/Grass-type move. Raises its Defense by 1 stage when hit by a Rock/Ground-type move. restores 1/4 of its maximum HP, rounded down, when hit by an Water/Grass/Rock/Ground-type move.",
+		shortDesc: "Raises Sp. Def and 25% restore when hit by Water/Grass moves. Raises Def and 25% restore when hit by Rock/Ground moves.",
+	},
+   octobrain: {
+		name: "Octobrain",
+		shortDesc: "This Pokemon's offensive stat is multiplied by 1.2 while using a Psychic-type attack. Ignores other Pokemon's stat changes when dealing damage.",
 	},
 	adaptability: {
 		name: "Adaptability",
