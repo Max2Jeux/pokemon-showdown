6fcfffbf9928269c1493267d0e4e51524431ee87
Max2Jeux <max.mercadie@iadfrance.fr>
2025-04-15 20:25:54 +0200

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -21233,11 +21233,12 @@ curtainsrepair: {
 		basePower: 120,
 		category: "Physical",
 		name: "Solar Effects",
+      weather: 'sunnyday',
 		pp: 15,
 		priority: 0,
 		flags: { contact: 1, protect: 1, mirror: 1, defrost: 1, metronome: 1 },
 		secondary: {
-			chance: 30,
+			chance: 20,
 			status: 'brn',
 		},
 		target: "normal",
