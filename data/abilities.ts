26b4bcff799deb4e8218951a9ac4bb1ac79c2058
Max2Jeux <max.mercadie@iadfrance.fr>
2025-09-14 22:32:41 +0200

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -3277,7 +3277,7 @@ omegalauncher: {
 			let randomStat: BoostID | undefined = stats.length ? this.sample(stats) : undefined;
 			if (randomStat) boost[randomStat] = 1;
 			this.boost(boost, pokemon, pokemon);
-      	this.heal(target.baseMaxhp / 4)
+      	this.heal(pokemon.baseMaxhp / 4);
 		},
 		flags: {},
 		name: "Stem Cell",
