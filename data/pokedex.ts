cdc9e886967d12571c5b768bc24694513236fcad
Max2Jeux <max.mercadie@iadfrance.fr>
2025-09-23 21:35:46 +0200

    Update pokedex.ts

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -13090,7 +13090,7 @@ tyruntkappa: {
 		baseForme: "50%",
 		types: ["Dragon", "Ground"],
 		gender: "N",
-		baseStats: { hp: 128, atk: 130, def: 121, spa: 81, spd: 105, spe: 100 },
+		baseStats: { hp: 128, atk: 130, def: 121, spa: 61, spd: 115, spe: 100 },
 		abilities: { 0: "Aura Break", S: "Power Construct" },
 		heightm: 5,
 		weightkg: 305,
