b37f037c396c9e0c347995f3ab14af2834607ba5
Max2Jeux <max.mercadie@iadfrance.fr>
2025-09-23 21:34:02 +0200

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -20788,6 +20788,32 @@ emotiveblush: {
 		zMove: { basePower: 180 },
 		contestType: "Beautiful",
 	},
+		judgmentarrows: {
+		num: 614,
+		accuracy: 100,
+		basePower: 110,
+		category: "Physical",
+		isNonstandard: "Past",
+		name: "Thousand Arrows",
+		pp: 10,
+		priority: 0,
+		flags: { protect: 1, mirror: 1, nonsky: 1 },
+		onEffectiveness(typeMod, target, type, move) {
+			if (move.type !== 'Ground') return;
+			if (!target) return; // avoid crashing when called from a chat plugin
+			// ignore effectiveness if the target is Flying type and immune to Ground
+			if (!target.runImmunity('Ground')) {
+				if (target.hasType('Flying')) return 0;
+			}
+		},
+		volatileStatus: 'smackdown',
+		ignoreImmunity: { 'Ground': true },
+		secondary: null,
+		target: "allAdjacentFoes",
+		type: "Ground",
+		zMove: { basePower: 180 },
+		contestType: "Beautiful",
+	},
 	thousandwaves: {
 		num: 615,
 		accuracy: 100,
