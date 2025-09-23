e898b33b4bf708e06f86304eda27f0f7dde60021
Max2Jeux <max.mercadie@iadfrance.fr>
2025-09-23 22:26:13 +0200

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -6529,7 +6529,6 @@ if (type === 'Flying') return 1;
 		name: "Solar Flare",
 		pp: 20,
 		priority: 0,
-      ignoreAbility: true,
 		flags: { protect: 1, mirror: 1, metronome: 1 },
 		onEffectiveness(typeMod, target, type) {
 if (type === 'Water') return 1;
